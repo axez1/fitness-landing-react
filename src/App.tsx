459a11796207728e405afc84cdd2f7c87fb3a2d2
Sergun Pavel <axez1@mail.ru>
2025-08-20 23:05:04 +0300

    start projct

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import NavBar from './components/nav';
+
+function App() {
+  return (
+    <>
+      <div className="app text-gray-500 bg-gray-50">
+        <NavBar />
+      </div>
+    </>
+  );
+}
+
+export default App;
